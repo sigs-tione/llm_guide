076940e8ac2ad58b9e44dd7a6480f88aefa6534e
mingjunzhou <mingjunzhou@tencent.com>
2023-12-06 19:56:16 +0800

    WIP: README.md

---
+++ run_baichuan2_7b_base.py
@@ -0,0 +1,13 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained(
+    "baichuan-inc/Baichuan2-7B-Base", use_fast=False, trust_remote_code=True
+)
+model = AutoModelForCausalLM.from_pretrained(
+    "baichuan-inc/Baichuan2-7B-Base", device_map="auto", trust_remote_code=True
+)
+inputs = tokenizer("登鹳雀楼->王之涣\n夜雨寄北->", return_tensors="pt")
+inputs = inputs.to("cuda:0")
+pred = model.generate(**inputs, max_new_tokens=64, repetition_penalty=1.1)
+print(tokenizer.decode(pred.cpu()[0], skip_special_tokens=True))
